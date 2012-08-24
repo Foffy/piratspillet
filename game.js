de44e8fe5682eab07cdc3cca3b6ddc01d935645a
Emil <emilovicmail@gmail.com>
2012-08-24 21:03:06 +0200

    clear

--- game.js
+++ game.js
@@ -22,67 +22,18 @@ function clear(){
 	ctx.fill();
 }
 
-
-var numCircles = 10, circles = [];
-
-for (var i = 0; i < numCircles; i++){
-	circles.push([Math.random() * width, Math.random() * height, Math.random() * 100, Math.random() / 2]);
+function pctOf(value,total){
+	return total/100*value;	
 }
 
-function DrawCircles(){
-	for(var i = 0; i < numCircles; i++){
-		ctx.fillStyle = 'rgba(255,255,255, ' + circles[i][3] + ')';
-		ctx.beginPath();
-		ctx.arc(circles[i][0],circles[i][1],circles[i][2],0,Math.PI * 2, true);
-		ctx.closePath();
-		ctx.fill();
-	}
-};
 
-function MoveCircles(deltaY){
-	for(var i = 0; i < numCircles; i++){
-		if(circles[i][1] < -circles[i][2]){
-			circles[i][0] = Math.random() * width;
-			circles[i][2] = Math.random() * 100;
-			circles[i][1] = height + circles[i][2];
-			circles[i][3] = Math.random() / 2;
-		}else{
-			circles[i][1] -= deltaY;
-		}
-	}
-}
 
-// player code
-function Player(){
-	this.image = new Image();
-	this.image.src = "icon_start.png";
-	this.width = 50;
-	this.height = 50;
-	this.X = 0;
-	this.Y = 0;
-	this.setPosition = function(x,y){
-		this.X = x;
-		this.Y = y;
-	}
-	
-	this.draw = function(){
-		try{
-			ctx.drawImage(this.image, this.X, this.Y);
-		}catch(e){
-			
-		}
-	}
-}
-var player = new Player();
-//player.setPosition((width-player.width)/2,(height - player.height)/2);
-player.setPosition(width - player.width, height - player.height);
+
 
 
 var GameLoop = function(){
 	clear();
-	MoveCircles(10);
-	DrawCircles();
-	player.draw();
+
 	setTimeout(GameLoop, 1000 / 50);
 }
 
