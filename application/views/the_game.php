bda0c23ce61964ffb9bb2af8925b2c88e5edc304
Foffy laptop <fof@flot.dk>
2012-11-20 23:19:11 +0100

    changing front page

--- application/views/the_game.php
+++ application/views/the_game.php
@@ -3,7 +3,7 @@
 <head>
   <title>Piratspillet </title>
 
-  <link type="text/css" rel="stylesheet" href="style/main.css"/>
+  <link type="text/css" rel="stylesheet" href="../../style/main.css"/>
 
   <meta charset="utf-8">
 
@@ -18,7 +18,7 @@
   <canvas id='c'></canvas>
 
   <div class="hidden">
-    <script src="script/game.js"></script>
+    <script src="../../script/game.js"></script>
   </div>
 
 </body>
