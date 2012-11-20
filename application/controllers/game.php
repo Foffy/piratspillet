3ee689c5a524eccd812e3155ec53195b3010ef1d
blaedel <blaedel@ubuntu.(none)>
2012-11-20 23:13:17 +0100

    testing live

---
+++ application/controllers/game.php
@@ -0,0 +1,14 @@
+<?php
+
+class Game extends CI_Controller {
+
+  public function index(){
+    $this->load->view('the_game');
+  }
+
+
+
+
+}
+
+?>
---
+++ application/views/the_game.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="da">
+<head>
+  <title>Piratspillet </title>
+
+  <link type="text/css" rel="stylesheet" href="style/main.css"/>
+
+  <meta charset="utf-8">
+
+  <!--[if lt IE 9]>
+  <script scr="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <link rel="stylesheet" href="/sites/all/themes/customtemplate/style/screenIE7.css">
+  <![endif]-->
+
+</head>
+<body>
+
+  <canvas id='c'></canvas>
+
+  <div class="hidden">
+    <script src="script/game.js"></script>
+  </div>
+
+</body>
+</html>
