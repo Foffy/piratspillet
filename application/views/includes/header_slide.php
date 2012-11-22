3dfd273cbbcc19bbfebf6dd39d7577709aaa2409
Foffy pc <foffy@pc>
2012-11-22 15:16:52 +0100

    more

--- application/views/includes/header_slide.php
+++ application/views/includes/header_slide.php
@@ -30,8 +30,7 @@ $(document).ready(function(){
 function onAfter() {
   $.getScript("http://beta.piratspillet.dk/application/views/includes/ruleTexts.js", function(){
     //var text = getRuleText("/assets/images/rule1.png");
-      $('#output').html("rly")
-        .append('<h3>' + this.alt + '</h3>');
+      $('#output').html("rly");
   });
 }
 
--- application/views/rules_page.php
+++ application/views/rules_page.php
@@ -7,6 +7,7 @@
 	</div>
 	<div class="slideshow">
 		<img src="/assets/images/rule1.png" width="450" height="450"/>
+		<img src="/assets/images/rule1.png" width="450" height="450"/>
 	</div>
 </div>
 <div id="contentBox">
