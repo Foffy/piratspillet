d67a6104b537cd600d71a68deab0b8b5755d4037
Foffy pc <foffy@pc>
2012-11-24 12:30:39 +0100

    alsk

--- application/views/includes/header_slide.php
+++ application/views/includes/header_slide.php
@@ -1,4 +1,4 @@
-<!doctype html>
+ <!doctype html>
 <html lang="da">
 <head>
   <title><?php echo $title; ?></title>
@@ -11,32 +11,13 @@
   <script scr="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <link rel="stylesheet" href="/sites/all/themes/customtemplate/style/screenIE7.css">
   <![endif]-->
-
+<script type="text/javascript" src="/assets/script/ruleTexts.js"></script>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.js"></script>
 <!-- include Cycle plugin -->
 <script type="text/javascript" src="http://cloud.github.com/downloads/malsup/cycle/jquery.cycle.all.latest.js"></script>
-<script type="text/javascript">
-$(document).ready(function(){
-    $('.slideshow').cycle({
-		fx: 'fade',
-    speed: 'fast',
-    timeout: 0,
-    next: '#next',
-    prev: '#prev',
-    after: onAfter
-	});
-});
+<!--<script type="text/javascript" src="/assets/script/game.js"></script>-->
 
-function onAfter() {
-  var text = "lol" + this.alt;
-  $.getScript("/application/views/includes/ruleTexts.js", function(){
-    
-  });
-  $('#output').html(text)
-      .append('<h3>' + this.alt + '</h3>');
-}
 
-</script>
 
 </head>
 <body>
--- application/views/rules_page.php
+++ application/views/rules_page.php
@@ -1,17 +1,5 @@
-<div id="topBox">
-</div>
-
-<div id="box">
-	<div id="output" align="left">
-	</div>
-	<div class="slideshow">
-		<img src="/assets/images/rule1.png" width="450" height="450" alt="1" />
-		<img src="/assets/images/logo.png" width="450" height="450" alt="2" />
-	</div>
-</div>
-<div id="contentBox">
-	<div class ="rape" align="right">
-		<a id="prev" href="#">&lt;        </a>
-		<a id="next" href="#">&gt;        </a>
+<div class="slideshow">
+	<div class="slide">
+		<img src="/assets/images/rule1.png" />
 	</div>
 </div>
