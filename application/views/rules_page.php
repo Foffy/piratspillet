402fab1a685067ef4550373cfa1c22deb138f3d3
Foffy pc <foffy@pc>
2012-11-26 00:19:39 +0100

    luls

--- application/views/rules_page.php
+++ application/views/rules_page.php
@@ -1,10 +1,12 @@
+<div id="box">
 <div class="slideOutput">
 	lol
 </div>
 <div class="slideshow">
-		<img src="/assets/images/rule1.png" width="450" height="450" alt="rule 1"/>
-		<img src="/assets/images/rule2.png" width="450" height="450" alt="rule 2"/>
+		<img src="/assets/images/rule1.png" width="450" height="150" alt="rule 1"/>
+		<img src="/assets/images/rule2.png" width="450" height="150" alt="rule 2"/>
 </div>
 <div class="nav">
 <a id="prev" href="#">Left</a>|<a id="next" href="#">Right</a>
+</div>
 </div>
