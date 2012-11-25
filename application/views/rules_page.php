6150bfc209ebf549d522bf596a5e3bcf2ea0aad8
Foffy pc <foffy@pc>
2012-11-25 20:40:26 +0100

    luls

--- application/views/rules_page.php
+++ application/views/rules_page.php
@@ -1,5 +1,7 @@
 <div class="slideshow">
-		<img src="/assets/images/rule1.png" />
-		<img src="/assets/images/rule2.png" />
+		<img src="/assets/images/rule1.png" width="450" height="450"/>
+		<img src="/assets/images/rule2.png" width="450" height="450"/>
 </div>
+<div class="nav">
 <a id="prev" href="#">Left</a>|<a id="next" href="#">Right</a>
+</div>
