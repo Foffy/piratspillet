4ead7e47c35f863ee22926b3a1a55a671c4cd216
Foffy pc <foffy@pc>
2012-11-26 00:57:30 +0100

    luls

--- application/views/includes/header_slide.php
+++ application/views/includes/header_slide.php
@@ -11,6 +11,19 @@
   <script scr="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <link rel="stylesheet" href="/sites/all/themes/customtemplate/style/screenIE7.css">
   <![endif]-->
+  <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-36589427-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.js"></script>
 <!-- include Cycle plugin -->
 <script type="text/javascript" src="http://cloud.github.com/downloads/malsup/cycle/jquery.cycle.all.latest.js"></script>
