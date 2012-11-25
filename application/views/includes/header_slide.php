f310a556e47e0cb5b3dea43509a32ab9f641bf77
Foffy pc <foffy@pc>
2012-11-25 20:42:10 +0100

    luls

--- application/views/includes/header_slide.php
+++ application/views/includes/header_slide.php
@@ -11,11 +11,11 @@
   <script scr="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <link rel="stylesheet" href="/sites/all/themes/customtemplate/style/screenIE7.css">
   <![endif]-->
-<script type="text/javascript" src="/assets/script/ruleTexts.js"></script>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.js"></script>
 <!-- include Cycle plugin -->
 <script type="text/javascript" src="http://cloud.github.com/downloads/malsup/cycle/jquery.cycle.all.latest.js"></script>
 <!--<script type="text/javascript" src="/assets/script/game.js"></script>-->
+<script type="text/javascript" src="/assets/script/ruleTexts.js"></script>
 
 
 
