a57621f99072c4ffb7f7b6442a92921cb444c966
Foffy pc <foffy@pc>
2012-11-26 22:37:00 +0100

    luls

--- application/views/rules_page.php
+++ application/views/rules_page.php
@@ -32,5 +32,7 @@
 	<a id="prev" href="#"><left><img src="/assets/images/arrowLeft.png" width="100"/></left></a><a id="next" href="#"><right><img src="/assets/images/arrow.png" width="100"/></right></a>
 	</div>
 	<div class="bottomBox">
-		<a href="http://www.beta.piratspillet.dk">Back</a>|<a href="http://www.beta.piratspillet.dk/index.php/game">Play</a>
+		<div class="speak4">
+			<a href="http://www.beta.piratspillet.dk">Back</a>|<a href="http://www.beta.piratspillet.dk/index.php/game">Play</a>
+		</div>
 	</div>
