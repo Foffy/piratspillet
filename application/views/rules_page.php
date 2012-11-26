359d3388058ba739932998986b661fa0d6ef442a
Foffy pc <foffy@pc>
2012-11-26 12:46:56 +0100

    luls

--- application/views/rules_page.php
+++ application/views/rules_page.php
@@ -7,10 +7,9 @@
 	</div></br>
 	<div class="slideshow" align="center">
 		<img src="/assets/images/rules/rule_1.png" width="800" height="150" alt="rule 1"/>
-		<img src="/assets/images/rules/rule2.png" width="800" height="150" alt="rule 2"/>
+		<img src="/assets/images/rules/rule_2.png" width="800" height="150" alt="rule 2"/>
 	</div>
 	</div>
 	<div class="nav" align="center">
 	<a id="prev" href="#"><left><img src="/assets/images/arrowLeft.png" width="100"/></left></a><a id="next" href="#"><right><img src="/assets/images/arrow.png" width="100"/></right></a>
-	
 	</div>
