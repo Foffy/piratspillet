7bf5fc1ffe793fcc552185af3a9468a5c87ff881
Foffy pc <foffy@pc>
2012-11-26 11:37:00 +0100

    luls

--- assets/script/ruleTexts.js
+++ assets/script/ruleTexts.js
@@ -19,7 +19,13 @@ function getRuleText(ruleIndex){
 	var textToHTML = "";
 	switch(ruleIndex){
 		case "rule 1":{
-			textToHTML = "<center><h1>Rule 1</h1></center></br> <p>This is rule number 1.</p>";
+			textToHTML = "<center><h1>Introduction</h1></center></br> <div class="speak4">Piratspillet is a drinking game where 2 to 8 players can compete 
+							</br> for the treasures of the far seas.
+							</br> To win the game one must obtain three of five Whore Coins,
+							</br> acquired by returning to harbour with five gold coins.
+							</br> It's illegal to drink when you're not supposed to and when
+							</br> caught doing so, you must take a penalty sip.
+						</div>";
 			break;
 		}
 		case "rule 2":{
