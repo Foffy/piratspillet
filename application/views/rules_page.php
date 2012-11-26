5b5e58f57e4944e6f734fe73175a49bd499497c0
Foffy pc <foffy@pc>
2012-11-26 12:42:26 +0100

    luls

--- application/views/rules_page.php
+++ application/views/rules_page.php
@@ -6,8 +6,8 @@
 	<div class="slideOutput">
 	</div></br>
 	<div class="slideshow" align="center">
-		<img src="/assets/images/rule1.png" width="800" height="150" alt="rule 1"/>
-		<img src="/assets/images/rule2.png" width="800" height="150" alt="rule 2"/>
+		<img src="/assets/images/rules/rule1.png" width="800" height="150" alt="rule 1"/>
+		<img src="/assets/images/rules/rule2.png" width="800" height="150" alt="rule 2"/>
 	</div>
 	</div>
 	<div class="nav" align="center">
