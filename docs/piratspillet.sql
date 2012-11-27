fc206109190d2d2114c66883ec7216e42a399611
EmilDesktop <emilovicmail@gmail.com>
2012-11-27 22:25:42 +0100

    sql

--- docs/piratspillet.sql
+++ docs/piratspillet.sql
@@ -1,10 +1,23 @@
-CREATE TABLE rolls (gameId int(6) NOT NULL, player varchar(25) NOT NULL, first datetime NOT NULL, last datetime NOT NULL, one int(6) NOT NULL, two int(6) NOT NULL, three int(6) NOT NULL, four int(6) NOT NULL, five int(6) NOT NULL, six int(6) NOT NULL, PRIMARY KEY (gameId,player));
+-- Drop if exists
+IF OBJECT_ID('rolls', 'U') IS NOT NULL
+  DROP TABLE rolls;
+IF OBJECT_ID('landed', 'U') IS NOT NULL
+  DROP TABLE landed;
+IF OBJECT_ID('activated', 'U') IS NOT NULL
+  DROP TABLE dbo.activated;
+IF OBJECT_ID('coins', 'U') IS NOT NULL
+  DROP TABLE coins;
+IF OBJECT_ID('sips', 'U') IS NOT NULL
+  DROP TABLE sips;
 
-CREATE TABLE landed (gameId int(6) NOT NULL, player varchar(25) NOT NULL, harbour int(6) NOT NULL, parrotTwo int(6) NOT NULL, skull int(6) NOT NULL, buyOne int(6) NOT NULL, mouth int(6) NOT NULL, parrotThree int(6) NOT NULL, chestOpen int(6) NOT NULL, buyTwo int(6) NOT NULL, exchange int(6) NOT NULL, steal int(6) NOT NULL, parrotFour int(6) NOT NULL, chestClosed int(6) NOT NULL, island int(6) NOT NULL, buyThree int(6) NOT NULL, PRIMARY KEY (gameId,player));
+-- Create tables
+CREATE TABLE rolls (gameId int(6) NOT NULL, player varchar(25) NOT NULL, first datetime NOT NULL, last datetime NOT NULL, one int(6) DEFAULT 0, two int(6) DEFAULT 0, three int(6) DEFAULT 0, four int(6) DEFAULT 0, five int(6) DEFAULT 0, six int(6) DEFAULT 0, PRIMARY KEY (gameId,player));
 
-CREATE TABLE activated (gameId int(6) NOT NULL, player varchar(25) NOT NULL, harbour int(6) NOT NULL, parrotTwo int(6) NOT NULL, skull int(6) NOT NULL, buyOne int(6) NOT NULL, mouth int(6) NOT NULL, parrotThree int(6) NOT NULL, chestOpen int(6) NOT NULL, buyTwo int(6) NOT NULL, exchange int(6) NOT NULL, steal int(6) NOT NULL, parrotFour int(6) NOT NULL, chestClosed int(6) NOT NULL, island int(6) NOT NULL, buyThree int(6) NOT NULL, PRIMARY KEY (gameId,player));
+CREATE TABLE landed (gameId int(6) NOT NULL, player varchar(25) NOT NULL, harbour int(6) DEFAULT 0, parrotTwo int(6) DEFAULT 0, skull int(6) DEFAULT 0, buyOne int(6) DEFAULT 0, mouth int(6) DEFAULT 0, parrotThree int(6) DEFAULT 0, chestOpen int(6) DEFAULT 0, buyTwo int(6) DEFAULT 0, exchange int(6) DEFAULT 0, steal int(6) DEFAULT 0, parrotFour int(6) DEFAULT 0, chestClosed int(6) DEFAULT 0, island int(6) DEFAULT 0, buyThree int(6) DEFAULT 0, PRIMARY KEY (gameId,player));
 
-CREATE TABLE coins (gameId int(6) NOT NULL, player varchar(25) NOT NULL, gold int(6) NOT NULL, silver int(6) NOT NULL, whore int(6) NOT NULL, PRIMARY KEY (gameId,player));
+CREATE TABLE activated (gameId int(6) NOT NULL, player varchar(25) NOT NULL, harbour int(6) DEFAULT 0, parrotTwo int(6) DEFAULT 0, skull int(6) DEFAULT 0, buyOne int(6) DEFAULT 0, mouth int(6) DEFAULT 0, parrotThree int(6) DEFAULT 0, chestOpen int(6) DEFAULT 0, buyTwo int(6) DEFAULT 0, exchange int(6) DEFAULT 0, steal int(6) DEFAULT 0, parrotFour int(6) DEFAULT 0, chestClosed int(6) DEFAULT 0, island int(6) DEFAULT 0, buyThree int(6) DEFAULT 0, PRIMARY KEY (gameId,player));
 
-CREATE TABLE sips (gameId int(6) NOT NULL, player varchar(25) NOT NULL, taken int(6) NOT NULL, given int(6) NOT NULL, PRIMARY KEY (gameId,player));
+CREATE TABLE coins (gameId int(6) NOT NULL, player varchar(25) NOT NULL, gold int(6) DEFAULT 0, silver int(6) DEFAULT 0, whore int(6) DEFAULT 0, PRIMARY KEY (gameId,player));
+
+CREATE TABLE sips (gameId int(6) NOT NULL, player varchar(25) NOT NULL, taken int(6) DEFAULT 0, given int(6) DEFAULT 0, PRIMARY KEY (gameId,player));
 
---
+++ docs/queries.sql
@@ -0,0 +1,29 @@
+-- ROLLS
+UPDATE rolls SET $field = $field + 1, last = $timestamp WHERE gameId = $gameId AND player = $player
+
+-- if 0 affected create
+INSERT INTO rolls (gameId,player,$field,first,last) VALUES ($gameId, $player, 1, $timestamp, $timestamp)
+
+-- LANDED
+UPDATE landed SET $field = $field + 1 WHERE gameId = @gameId AND player = $player
+
+-- if 0 affected create
+INSERT INTO landed (gameId,player,$field) VALUES ($gameId, $player, 1)
+
+-- ACTIVATED
+UPDATE activated SET $field = $field + 1 WHERE gameId = $gameId AND player = $player
+
+-- if 0 affected create
+INSERT INTO activated (gameId,player,$field) VALUES ($gameId, $player, 1)
+
+-- COINS
+UPDATE coins SET gold = gold + $gold, silver = silver + $silver, whore = whore + $whore WHERE gameId = $gameId AND player = $player
+
+-- if 0 affected create
+INSERT INTO coins (gameId, player, gold, silver, whore) VALUES ($gameId, $player, $gold, $silver, $whore)
+
+-- SIPS
+UPDATE sips SET taken = taken + $taken, given = given + $given WHERE gameId = $gameId AND player = $player
+
+-- if 0 affected create
+INSERT INTO sips (gameId, player, taken, given) VALUES ($gameId, $player, $taken, $given)
