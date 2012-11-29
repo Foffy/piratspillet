b57d09b06eb1b048ab5ade14769d398f211ef17e
Foffy pc <foffy@pc>
2012-11-29 15:59:34 +0100

    db

--- assets/script/update.js
+++ assets/script/update.js
@@ -1,1 +1,5 @@
-$.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['1','John', 'two']});
+$.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['rolls','1', 'Foffy', 'two']});
+$.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['landed','1', 'Foffy', 'buyOne']});
+$.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['activated','1', 'Emil', 'chestOpen']});
+$.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['coins','1', 'Foffy', '2','3','1']});
+$.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['sips','1', 'Emil', '9001', '4']});
