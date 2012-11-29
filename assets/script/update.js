d06bdc597daa78409eac885f284b2ec1078a6696
Foffy pc <foffy@pc>
2012-11-29 17:11:35 +0100

    trying for debug db

--- assets/script/update.js
+++ assets/script/update.js
@@ -1,3 +1,4 @@
+$.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['games']});
 $.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['rolls','1', 'Foffy', 'two']});
 $.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['landed','1', 'Foffy', 'buyOne']});
 $.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['activated','1', 'Emil', 'chestOpen']});
