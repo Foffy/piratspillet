5230329341bc4ca5a1c7d631bc6e4f8e8c378517
Foffy pc <foffy@pc>
2012-11-29 14:28:14 +0100

    updated rules

--- application/controllers/update_db.php
+++ application/controllers/update_db.php
@@ -4,16 +4,12 @@ class Update_db extends CI_Controller {
 
 	public function index(){
 		$data = $this->input->post('data');
-		updateRolls($data);
-
-	}
-
-	private function updateRolls($data){
+		
 		$dbData = array(
 			'gameId' => $data[0],
 			'player' => $data[1],
 			);
 
 		$this->db->insert('rolls',$dbData);
+
 	}
-}
