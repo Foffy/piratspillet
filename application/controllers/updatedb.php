e67de737f56ec05edc89b42f0aa2befebd61b944
Foffy pc <foffy@pc>
2012-11-29 15:06:58 +0100

    db

--- application/controllers/updatedb.php
+++ application/controllers/updatedb.php
@@ -12,18 +12,21 @@ class Updatedb extends CI_Controller {
 	private function updateRolls($data){
 		$dbData = array(
 			'player' => $data[1],
-			$data[2] => $data[2]+"+1"
+			$data[2] => $data[2]+"+1",
+			'last' => "NOW()"
 			);
 
 		$this->db->where('gameID',$data[0]);
 		$this->db->update('rolls',$dbData);
 		$check = $this->db->affected_rows();
-		
+
 		if($check==0){
 			$dbData = array(
 				'gameID' => $data[0],
 				'player' => $data[1],
-				$data[2] => $data[2]+"+1"
+				$data[2] => $data[2]+"+1",
+				'first' => "NOW()",
+				'last' => "NOW()"
 				);
 			$this->db->insert('rolls',$dbData);
 		}
