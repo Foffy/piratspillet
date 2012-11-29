feb378ce93ba0bc28b2f3cf0f73a15a9707fb0fe
Foffy pc <foffy@pc>
2012-11-29 17:13:19 +0100

    trying for debug db

--- assets/script/update.js
+++ assets/script/update.js
@@ -1,6 +1,6 @@
-$.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['games']});
-$.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['rolls','1', 'Foffy', 'two']});
-$.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['landed','1', 'Foffy', 'buyOne']});
-$.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['activated','1', 'Emil', 'chestOpen']});
-$.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['coins','1', 'Foffy', '2','3','1']});
-$.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['sips','1', 'Emil', '9001', '4']});
+$.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['debug','games']});
+$.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['debug','rolls','1', 'Foffy', 'two']});
+$.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['debug','landed','1', 'Foffy', 'buyOne']});
+$.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['debug','activated','1', 'Emil', 'chestOpen']});
+$.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['debug','coins','1', 'Foffy', '2','3','1']});
+$.post("http://beta.piratspillet.dk/index.php/updatedb", {'data':['debug','sips','1', 'Emil', '9001', '4']});
