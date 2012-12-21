ef331f80b906e52a835fa880538dd48846d35fe2
Foffy laptop <fof@flot.dk>
2012-12-21 14:38:29 +0100

    trying to get dateTime to work

--- application/views/splash.php
+++ application/views/splash.php
@@ -10,5 +10,15 @@
   		<a href="http://beta.piratspillet.dk/index.php/game" title="Play the game!">PLAY</a>
   		</br> 
   		<a href="http://beta.piratspillet.dk/index.php/rules" title="Play the game!">READ THE RULES</a>
+  		</br>
+  		Time is: <?php
+		date_default_timezone_set('EST');
+		$date = new DateTime();
+		echo $date->getTimestamp();
+		echo "<br>";
+		echo $dateTime->format("Y-m-d H:i:s");
+
+ 		?>
+
 	</div>
 </div>
