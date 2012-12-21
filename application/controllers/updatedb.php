72b1c3ee555f90096ebfbd10e7f492048967f190
Foffy laptop <fof@flot.dk>
2012-12-21 15:06:20 +0100

    trying to get dateTime to work

--- application/controllers/updatedb.php
+++ application/controllers/updatedb.php
@@ -45,10 +45,11 @@ class Updatedb extends CI_Controller {
 
 		date_default_timezone_set('CET');
 		$date = new DateTime();
+		$browserInfo = $this->getBrowser();
 
 		$dbData = array(
 			'ip' => $_SERVER['REMOTE_ADDR'],
-			'browser' => $this->getUserBrowser(),
+			'browser' => $browserInfo['name']+" "+$browserInfo['version'],
 			'started' => $date->format("Y-m-d H:i:s")
 			);
 		$this->db->insert($data[1],$dbData);
@@ -154,35 +155,90 @@ class Updatedb extends CI_Controller {
 		}
 	}
 
-	function getUserBrowser() 
+	function getBrowser() 
 	{ 
 	    $u_agent = $_SERVER['HTTP_USER_AGENT']; 
-	    $ub = ''; 
-	    if(preg_match('/MSIE/i',$u_agent)) 
+	    $bname = 'Unknown';
+	    $platform = 'Unknown';
+	    $version= "";
+
+	    //First get the platform?
+	    if (preg_match('/linux/i', $u_agent)) {
+	        $platform = 'linux';
+	    }
+	    elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
+	        $platform = 'mac';
+	    }
+	    elseif (preg_match('/windows|win32/i', $u_agent)) {
+	        $platform = 'windows';
+	    }
+	    
+	    // Next get the name of the useragent yes seperately and for good reason
+	    if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent)) 
 	    { 
-	        $ub = "ie"; 
+	        $bname = 'Internet Explorer'; 
+	        $ub = "MSIE"; 
 	    } 
 	    elseif(preg_match('/Firefox/i',$u_agent)) 
 	    { 
-	        $ub = "firefox"; 
-	    } 
-	    elseif(preg_match('/Safari/i',$u_agent)) 
-	    { 
-	        $ub = "safari"; 
+	        $bname = 'Mozilla Firefox'; 
+	        $ub = "Firefox"; 
 	    } 
 	    elseif(preg_match('/Chrome/i',$u_agent)) 
 	    { 
-	        $ub = "chrome"; 
+	        $bname = 'Google Chrome'; 
+	        $ub = "Chrome"; 
 	    } 
-	    elseif(preg_match('/Flock/i',$u_agent)) 
+	    elseif(preg_match('/Safari/i',$u_agent)) 
 	    { 
-	        $ub = "flock"; 
+	        $bname = 'Apple Safari'; 
+	        $ub = "Safari"; 
 	    } 
 	    elseif(preg_match('/Opera/i',$u_agent)) 
 	    { 
-	        $ub = "opera"; 
+	        $bname = 'Opera'; 
+	        $ub = "Opera"; 
+	    } 
+	    elseif(preg_match('/Netscape/i',$u_agent)) 
+	    { 
+	        $bname = 'Netscape'; 
+	        $ub = "Netscape"; 
 	    } 
 	    
-	    return $ub; 
+	    // finally get the correct version number
+	    $known = array('Version', $ub, 'other');
+	    $pattern = '#(?<browser>' . join('|', $known) .
+	    ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
+	    if (!preg_match_all($pattern, $u_agent, $matches)) {
+	        // we have no matching number just continue
+	    }
+	    
+	    // see how many we have
+	    $i = count($matches['browser']);
+	    if ($i != 1) {
+	        //we will have two since we are not using 'other' argument yet
+	        //see if version is before or after the name
+	        if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
+	            $version= $matches['version'][0];
+	        }
+	        else {
+	            $version= $matches['version'][1];
+	        }
+	    }
+	    else {
+	        $version= $matches['version'][0];
+	    }
+	    
+	    // check if we have a number
+	    if ($version==null || $version=="") {$version="?";}
+	    
+	    return array(
+	        'userAgent' => $u_agent,
+	        'name'      => $bname,
+	        'version'   => $version,
+	        'platform'  => $platform,
+	        'pattern'    => $pattern
+	    );
 	} 
+
 }
