282363b56fa38a5d0ca1181ec8f2ed9346ef1826
Foffy laptop <fof@flot.dk>
2013-01-01 17:17:10 +0100

    setting up test page for db testing

---
+++ application/views/testdb.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="da">
+<head>
+  <title><?php echo $title; ?></title>
+
+  <link type="text/css" rel="stylesheet" href="/assets/style/main.css"/>
+
+  <meta charset="utf-8">
+
+  <!--[if lt IE 9]>
+  <script scr="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <link rel="stylesheet" href="/sites/all/themes/customtemplate/style/screenIE7.css">
+  <![endif]-->
+  <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-36589427-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>
+
+</head>
+<body>
+
+<h1>yO!</h1>
+
+</body>
+</html>
