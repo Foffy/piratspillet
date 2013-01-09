558760f8b30dfebce940775914703e7913b1653b
Foffy <blc704@alumni.ku.dk>
2013-01-09 23:59:41 +0100

    Testing db update

--- application/controllers/updatedb.php
+++ application/controllers/updatedb.php
@@ -57,7 +57,7 @@ class Updatedb extends CI_Controller {
 		$date = new DateTime();
 
 		$dbData = array(
-			"".$data[4] => "four+1",
+			$data[4] => "'four'+1",
 			'last' => $date->format("Y-m-d H:i:s")
 			);
 
@@ -68,7 +68,7 @@ class Updatedb extends CI_Controller {
 			$dbData = array(
 				'gameId' => $data[2],
 				'player' => $data[3],
-				$data[4] => $data[4]+"+1",
+				.$data[4] => $data[4]." + 1",
 				'first' => $date->format("Y-m-d H:i:s"),
 				'last' => $date->format("Y-m-d H:i:s")
 				);
