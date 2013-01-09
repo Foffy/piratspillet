363da929825fff429afa7821a2e86c126da699b6
Foffy <blc704@alumni.ku.dk>
2013-01-10 00:15:42 +0100

    Testing db update

--- application/controllers/updatedb.php
+++ application/controllers/updatedb.php
@@ -56,7 +56,7 @@ class Updatedb extends CI_Controller {
 		date_default_timezone_set('CET');
 		$date = new DateTime();
 
-		$incr = "1";#"" . $data[4] . " + 1";
+		$incr = "" . $data[4] . " + 1";
 		$column = "" . $data[4];
 
 		$this->db->set($column,$incr,FALSE);
