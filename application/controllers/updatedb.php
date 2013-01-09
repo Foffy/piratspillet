e6905b3d79e7f8399afbb361b2d9aaf01b3a1afa
Foffy <blc704@alumni.ku.dk>
2013-01-09 23:14:34 +0100

    Testing db update

--- application/controllers/updatedb.php
+++ application/controllers/updatedb.php
@@ -61,14 +61,16 @@ class Updatedb extends CI_Controller {
 			'last' => $date->format("Y-m-d H:i:s")
 			);
 
-		$check = $this->db->where('gameId',$data[2]).where('player',$data[3]);
-		$check = $this->db->update($data[1],$dbData);
+		$this->db->where('gameId',$data[2]).where('player',$data[3]);
+		$this->db->update($data[1],$dbData);
+		$check = $this->db->affected_rows();
 
 
 		//$check = $this->db->query("UPDATE rolls SET "+$data[4]+" = "+$data[4]+" + 1  WHERE gameId = "+$data[2]+" AND player = "+$data[3]);
 		//$check = $this->db->query("UPDATE rolls SET '".$data[4]."' = '".$data[4]."' + 1, 'last' = '".$date->format("Y-m-d H:i:s")."' WHERE 'gameId' = '".$data[2]."' AND player = '".$data[3]."'");
 		//$check = $this->updateDatabase($data, $dbData);
 		if($check==0){
+
 
 			$dbData = array(
 				'gameId' => $data[2],
