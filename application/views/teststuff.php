8c391a6aa9767a1b40d0f67609e415f1e5b17d86
Foffy laptop <fof@flot.dk>
2013-01-10 22:04:48 +0100

    new twitter

--- application/views/teststuff.php
+++ application/views/teststuff.php
@@ -40,7 +40,7 @@
           $this->db->limit('10');
           $query = $this->db->get('sips');
 
-          echo "<table><th><strong>Player</strong></th><th>Taken</th><th>Given</th>";
+          echo "<table><tr><th><strong>Player</strong></th><th>Taken</th><th>Given</th></tr>";
           echo "<hr>";
 
           foreach ($query->result() as $row)
