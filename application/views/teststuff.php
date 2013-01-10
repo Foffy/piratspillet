460f15466a7e0d828d5799220a0afd945992e709
Foffy laptop <fof@flot.dk>
2013-01-10 04:40:05 +0100

    new front page test

--- application/views/teststuff.php
+++ application/views/teststuff.php
@@ -31,7 +31,7 @@
           $query = $this->db->get('sips');
           foreach ($query->result() as $row)
           {
-            echo $row->gameid . " " . $row->player . "" . $row->taken;
+            echo $row->gameid . " " . $row->player . "" . $row->taken . "</br>";
           }
           
           ?>
