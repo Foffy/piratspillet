99e68ead02bde77a9d759147efb2e7476eaed3b3
Foffy laptop <fof@flot.dk>
2013-01-10 21:07:16 +0100

    new twitter

--- application/views/teststuff.php
+++ application/views/teststuff.php
@@ -60,44 +60,11 @@
 
   </div>
 
-  <div class="middleContainer" align="center">
-    <div class="box">
-      <div class="counterBox">
-      </div>
-      <div class="slideOutput">
-      </div>
-      <div class="slideshow" align="center">
-        <img src="/assets/images/rules/rule_1.png" width="700" height="150" alt="1"/>
-        <img src="/assets/images/rules/rule_2.png" width="700" height="150" alt="2"/>
-        <img src="/assets/images/rules/rule_3.png" width="700" height="150" alt="3"/>
-        <img src="/assets/images/rules/rule_4.png" width="700" height="150" alt="4"/>
-        <img src="/assets/images/rules/rule_5.png" width="700" height="150" alt="5"/>
-        <img src="/assets/images/rules/rule_6.png" width="700" height="150" alt="6"/>
-        <img src="/assets/images/rules/rule_7.png" width="700" height="150" alt="7"/>
-        <img src="/assets/images/rules/rule_8.png" width="700" height="150" alt="8"/>
-        <img src="/assets/images/rules/rule_9.png" width="700" height="150" alt="9"/>
-        <img src="/assets/images/rules/rule_10.png" width="700" height="150" alt="10"/>
-        <img src="/assets/images/rules/rule_11.png" width="700" height="150" alt="11"/>
-        <img src="/assets/images/rules/rule_12.png" width="700" height="150" alt="12"/>
-        <img src="/assets/images/rules/rule_13.png" width="700" height="150" alt="13"/>
-        <img src="/assets/images/rules/rule_14.png" width="700" height="150" alt="14"/>
-        <img src="/assets/images/rules/rule_15.png" width="700" height="150" alt="15"/>
-        <img src="/assets/images/rules/rule_16.png" width="700" height="150" alt="16"/>
-        <img src="/assets/images/rules/rule_17.png" width="700" height="150" alt="17"/>
-        <img src="/assets/images/rules/rule_18.png" width="700" height="150" alt="18"/>
-        <img src="/assets/images/rules/rule_19.png" width="700" height="150" alt="19"/>
-        <img src="/assets/images/rules/rule_20.png" width="700" height="150" alt="20"/>
-      </div>
-      </div>
-      <div class="nav" align="center">
-      <a id="prev" href="#"><left><img src="/assets/images/arrowLeft.png" width="100"/></left></a><a id="next" href="#"><right><img src="/assets/images/arrow.png" width="100"/></right></a>
-      </div>
-
   <a href="http://beta.piratspillet.dk/index.php/rules">
-   <img src="/assets/images/rules.jpg" width="100" height="100" />
+   <img src="/assets/images/rules.jpg" />
   </a>
   <a href="http://beta.piratspillet.dk/index.php/game" >
-   <img src="/assets/images/play.jpg" width="100" height="100" />
+   <img src="/assets/images/play.png" />
   </a>
 
 </div>
