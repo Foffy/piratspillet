82ff4a24ed61d75a0c6758ffb7b70303bc17e745
Foffy laptop <fof@flot.dk>
2013-01-10 17:59:53 +0100

    new twitter

--- assets/script/tweetMachine.js
+++ assets/script/tweetMachine.js
@@ -39,7 +39,7 @@
 					autoRefresh:    true, // CURRENTLY REQUIRED. Auto-refresh the tweets
 					animateOut:     false, // NOT YET SUPPORTED. Animate out old tweets.
 					animateIn:      true, // Fade in new tweets.
-					tweetFormat: "<li class='tweet'><a href='' class='username'></a><a href='' class='time'></a></div><p class='content'></p></li>", // Format for each tweet
+					tweetFormat: "<li class='tweet'><img class='avatar' src=''/><div class='meta'><a href='' class='username'></a><a href='' class='time'></a></div><p class='content'></p></li>", // Format for each tweet
 					localization: { // Verbiage to use for timestamps
 						seconds:    'seconds ago',
 						minute:     'a minute ago',
