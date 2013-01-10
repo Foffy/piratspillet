0f47328baee8e4b01d9e15b4d1481aeaf5cc0fe9
Foffy laptop <fof@flot.dk>
2013-01-10 21:38:27 +0100

    new twitter

--- application/views/teststuff.php
+++ application/views/teststuff.php
@@ -40,11 +40,13 @@
           $this->db->limit('10');
           $query = $this->db->get('sips');
 
+          echo "<table vertical_align="middle">";
 
           foreach ($query->result() as $row)
           {
-            echo "<strong>" . $row->player . " drank :</strong>" . $row->taken . " sips and gave ".$row->given ."</br>";
+            echo "<tr><td><strong>" . $row->player . " drank :</strong></td><td>" . $row->taken . " sips and gave </td><td>".$row->given ."</td></tr></br>";
           }
+          echo "</table>";
           
           ?>
         </div>
