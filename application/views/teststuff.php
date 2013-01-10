04756e2e8844e439614204b1aa8204107d3a01b8
Foffy laptop <fof@flot.dk>
2013-01-10 17:35:47 +0100

    new twitter

--- application/views/teststuff.php
+++ application/views/teststuff.php
@@ -11,7 +11,7 @@
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="/assets/script/vendor/jquery-1.8.2.min.js"><\/script>')</script>
-    <script src="js/tweetMachine.js"></script>
+    <script src="/assets/script/tweetMachine.js"></script>
 
     <script>
         $('.tweets').tweetMachine('#piratspillet');
