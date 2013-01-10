ada205c8e54fcd09201566523897dc68237036ee
Foffy laptop <fof@flot.dk>
2013-01-10 19:08:46 +0100

    new twitter

--- application/views/teststuff.php
+++ application/views/teststuff.php
@@ -9,7 +9,6 @@
     <h2>Activity</h2>
     <ol class="tweets"></ol>
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="/assets/script/vendor/jquery-1.8.2.min.js"><\/script>')</script>
     <script src="/assets/script/tweetMachine.js"></script>
 
@@ -17,7 +16,7 @@
         $('.tweets').tweetMachine('#piratspillet');
     </script>
   </div>
-
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
   <div class="contentCol"
 
     <div class="rightContainer">
