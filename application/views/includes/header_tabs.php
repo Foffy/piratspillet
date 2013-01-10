76c6f4ee2dbfd87fdff71be955424cabce68b163
Foffy laptop <fof@flot.dk>
2013-01-10 19:12:58 +0100

    new twitter

--- application/views/includes/header_tabs.php
+++ application/views/includes/header_tabs.php
@@ -11,8 +11,8 @@
   <script scr="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <link rel="stylesheet" href="/sites/all/themes/customtemplate/style/screenIE7.css">
   <![endif]-->
-
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+  
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
   <script src="http://cdn.jquerytools.org/1.2.7/full/jquery.tools.min.js"></script>
   <link rel="stylesheet" href="/assets/style/tabs.css"
         type="text/css" media="screen" />
