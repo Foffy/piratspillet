f3240191444de0202884f7e11497496ea2176fc7
Foffy laptop <fof@flot.dk>
2013-01-10 22:53:27 +0100

    new twitter

--- application/views/teststuff.php
+++ application/views/teststuff.php
@@ -39,12 +39,14 @@
           $this->db->order_by('given', 'DESC');
           $this->db->limit('20');
           $query = $this->db->get('sips');
+          $count = 1;
 
           echo "<table><tr><th>Rank</th><th><strong>Player</strong></th><th>Taken</th><th>Given</th></tr>";
 
           foreach ($query->result() as $row)
           {
-            echo "<tr><td><strong>" . $row->player . "</strong></td><td>" . $row->taken . "</td><td>".$row->given . "</td></tr>";
+            echo "<tr><td>".$count."</td><td><strong>" . $row->player . "</strong></td><td>" . $row->taken . "</td><td>".$row->given . "</td></tr>";
+            $count += 1;
           }
           echo "</table>";
           
