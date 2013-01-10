5f3470fbbf0df5368d21b4bc944381db9b6fd6d3
Foffy laptop <fof@flot.dk>
2013-01-10 03:50:46 +0100

    new front page test

--- application/controllers/teststuff.php
+++ application/controllers/teststuff.php
@@ -1,82 +1,18 @@
-<!doctype html>
-<html lang="da">
-<head>
-  <title>"Piratspillet - DEBUG!"</title>
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-  <link type="text/css" rel="stylesheet" href="/assets/style/main.css"/>
+class Teststuff extends CI_Controller {
 
-  <meta charset="utf-8">
+  public function index(){
 
-  <!--[if lt IE 9]>
-  <script scr="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-  <link rel="stylesheet" href="/sites/all/themes/customtemplate/style/screenIE7.css">
-  <![endif]-->
+    $data['header'] = "includes/header_tabs";
+    $data['title'] = "Piratspillet - DEBUG!";
+    $data['main_content'] = "teststuff";
+    $this->load->view('includes/template', $data);
+  }
+}
 
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-  <script src="http://cdn.jquerytools.org/1.2.7/full/jquery.tools.min.js"></script>
-  <link rel="stylesheet" href="/assets/style/tabs.css"
-        type="text/css" media="screen" />
-  <link rel="stylesheet" href="/assets/style/tabs-panes.css"
-        type="text/css" media="screen" />
-</head>
-<div class="topBox">
-  <h1>Piratspillet!</h1>
-  <hr>
-</div>
+?>
 
-
-<div class="globalContainer">
-  <div class="leftContainer">
-    </br>
-    <h2>Activity</h2>
-    <div class="textStuff">
-      <?php
-        echo "Work. Now!";
-      ?>
-    </div>
-  </div>
-
-  <div class="contentCol">
-
-    <div class="rightContainer">
-      <h2>Leaderboards</h2>
-      <ul class="tabs">
-        <li><a href="#">Wins</a></li>
-        <li><a href="#">Coins</a></li>
-        <li><a href="#">Sips</a></li>
-      </ul>
-
-      <!-- tab "panes" -->
-      <div class="panes">
-        <div class="textStuff">
-        </div>
-        <div>Second tab content</div>
-        <div>Third tab content</div>
-      </div>
-
-      <script>
-      // perform JavaScript after the document is scriptable.
-      $(function() {
-          // setup ul.tabs to work as tabs for each div directly under div.panes
-          $("ul.tabs").tabs("div.panes > div");
-      });
-      </script>       
-    </div>
-
-
-  </div>
-
-  <div class="middleContainer" align="center">
-
-  <a href="http://beta.piratspillet.dk/index.php/rules">
-   <img src="/assets/images/rules.jpg" width="100" height="100" />
-  </a>
-  <a href="http://beta.piratspillet.dk/index.php/game" >
-   <img src="/assets/images/play.jpg" width="100" height="100" />
-  </a>
-
-</div>
-</div>
 
 </body>
 </html>
